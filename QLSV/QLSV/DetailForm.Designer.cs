c65a80927f2801ed0ff8955da17c65a811786fcc
tosanoob <89732559+tosanoob@users.noreply.github.com>
2023-02-19 18:43:55 +0700

    Add project QLSV
    BTVN - 20/2 nộp

---
+++ QLSV/QLSV/DetailForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace QLSV
+{
+    partial class DetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMSSV = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelLSH = new System.Windows.Forms.Label();
+            this.labelNS = new System.Windows.Forms.Label();
+            this.labelDTB = new System.Windows.Forms.Label();
+            this.textBoxMSSV = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxDTB = new System.Windows.Forms.TextBox();
+            this.comboBoxLSH = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerNS = new System.Windows.Forms.DateTimePicker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMSSV
+            // 
+            this.labelMSSV.AutoSize = true;
+            this.labelMSSV.Location = new System.Drawing.Point(34, 40);
+            this.labelMSSV.Name = "labelMSSV";
+            this.labelMSSV.Size = new System.Drawing.Size(37, 13);
+            this.labelMSSV.TabIndex = 0;
+            this.labelMSSV.Text = "MSSV";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(34, 101);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(26, 13);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Tên";
+            // 
+            // labelLSH
+            // 
+            this.labelLSH.AutoSize = true;
+            this.labelLSH.Location = new System.Drawing.Point(34, 164);
+            this.labelLSH.Name = "labelLSH";
+            this.labelLSH.Size = new System.Drawing.Size(71, 13);
+            this.labelLSH.TabIndex = 2;
+            this.labelLSH.Text = "Lớp sinh hoạt";
+            // 
+            // labelNS
+            // 
+            this.labelNS.AutoSize = true;
+            this.labelNS.Location = new System.Drawing.Point(34, 226);
+            this.labelNS.Name = "labelNS";
+            this.labelNS.Size = new System.Drawing.Size(54, 13);
+            this.labelNS.TabIndex = 3;
+            this.labelNS.Text = "Ngày sinh";
+            // 
+            // labelDTB
+            // 
+            this.labelDTB.AutoSize = true;
+            this.labelDTB.Location = new System.Drawing.Point(34, 283);
+            this.labelDTB.Name = "labelDTB";
+            this.labelDTB.Size = new System.Drawing.Size(81, 13);
+            this.labelDTB.TabIndex = 4;
+            this.labelDTB.Text = "Điểm trung bình";
+            // 
+            // textBoxMSSV
+            // 
+            this.textBoxMSSV.Location = new System.Drawing.Point(133, 37);
+            this.textBoxMSSV.Name = "textBoxMSSV";
+            this.textBoxMSSV.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMSSV.TabIndex = 5;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(133, 98);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 6;
+            // 
+            // textBoxDTB
+            // 
+            this.textBoxDTB.Location = new System.Drawing.Point(133, 280);
+            this.textBoxDTB.Name = "textBoxDTB";
+            this.textBoxDTB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDTB.TabIndex = 8;
+            // 
+            // comboBoxLSH
+            // 
+            this.comboBoxLSH.FormattingEnabled = true;
+            this.comboBoxLSH.Location = new System.Drawing.Point(133, 160);
+            this.comboBoxLSH.Name = "comboBoxLSH";
+            this.comboBoxLSH.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxLSH.TabIndex = 9;
+            // 
+            // dateTimePickerNS
+            // 
+            this.dateTimePickerNS.Location = new System.Drawing.Point(133, 220);
+            this.dateTimePickerNS.Name = "dateTimePickerNS";
+            this.dateTimePickerNS.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerNS.TabIndex = 10;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(488, 36);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Nam";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(488, 69);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Nữ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(416, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Giới tính";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(488, 160);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(45, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Ảnh";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(488, 199);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(61, 17);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Học bạ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(488, 239);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(55, 17);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "CCCD";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(416, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Hồ sơ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(533, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // DetailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 331);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dateTimePickerNS);
+            this.Controls.Add(this.comboBoxLSH);
+            this.Controls.Add(this.textBoxDTB);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxMSSV);
+            this.Controls.Add(this.labelDTB);
+            this.Controls.Add(this.labelNS);
+            this.Controls.Add(this.labelLSH);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.labelMSSV);
+            this.Name = "DetailForm";
+            this.Text = "DetailForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMSSV;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelLSH;
+        private System.Windows.Forms.Label labelNS;
+        private System.Windows.Forms.Label labelDTB;
+        private System.Windows.Forms.TextBox textBoxMSSV;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxDTB;
+        private System.Windows.Forms.ComboBox comboBoxLSH;
+        private System.Windows.Forms.DateTimePicker dateTimePickerNS;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ QLSV/QLSV/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace QLSV
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLSH = new System.Windows.Forms.Label();
+            this.comboBoxLSH = new System.Windows.Forms.ComboBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.datashow = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.comboBoxSortOption = new System.Windows.Forms.ComboBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.datashow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelLSH
+            // 
+            this.labelLSH.AutoSize = true;
+            this.labelLSH.Location = new System.Drawing.Point(32, 34);
+            this.labelLSH.Name = "labelLSH";
+            this.labelLSH.Size = new System.Drawing.Size(77, 13);
+            this.labelLSH.TabIndex = 0;
+            this.labelLSH.Text = "Lớp sinh hoạt: ";
+            // 
+            // comboBoxLSH
+            // 
+            this.comboBoxLSH.FormattingEnabled = true;
+            this.comboBoxLSH.Location = new System.Drawing.Point(115, 31);
+            this.comboBoxLSH.Name = "comboBoxLSH";
+            this.comboBoxLSH.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLSH.TabIndex = 1;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(390, 31);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(162, 20);
+            this.textBoxSearch.TabIndex = 5;
+            // 
+            // datashow
+            // 
+            this.datashow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datashow.Location = new System.Drawing.Point(35, 97);
+            this.datashow.Name = "datashow";
+            this.datashow.Size = new System.Drawing.Size(617, 355);
+            this.datashow.TabIndex = 6;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(35, 505);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(161, 505);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
+            this.buttonEdit.TabIndex = 8;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(285, 505);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(407, 505);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 10;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxSortOption
+            // 
+            this.comboBoxSortOption.FormattingEnabled = true;
+            this.comboBoxSortOption.Location = new System.Drawing.Point(531, 505);
+            this.comboBoxSortOption.Name = "comboBoxSortOption";
+            this.comboBoxSortOption.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSortOption.TabIndex = 11;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(577, 29);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 12;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 575);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.comboBoxSortOption);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.datashow);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.comboBoxLSH);
+            this.Controls.Add(this.labelLSH);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.datashow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLSH;
+        private System.Windows.Forms.ComboBox comboBoxLSH;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.DataGridView datashow;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.ComboBox comboBoxSortOption;
+        private System.Windows.Forms.Button buttonSearch;
+    }
+}
+
